f051b61a8ed74bdb6b66b6320672e605a1910cfb
Amanda Jo Griffin <amandajogriffin@Amandas-Air.lan>
2023-11-08 10:15:31 -0500

    adding area of triangle

---
+++ area_of_triangle.py
@@ -0,0 +1,6 @@
+#create a function that will return the area of a triangle given base and height
+
+def area_of_triangle(base, height):
+    return base * height * 0.5
+
+print(area_of_triangle(5,4))
